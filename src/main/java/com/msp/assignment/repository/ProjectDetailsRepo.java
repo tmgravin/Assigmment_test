091e5a274d68d898f103748e991393aeab5e5931
RANAS7 <deepakrana.dr537@gmail.com>
2024-07-31 12:02:26 +0545

    get projet with projectDEtails and users

--- src/main/java/com/msp/assignment/repository/ProjectDetailsRepo.java
+++ src/main/java/com/msp/assignment/repository/ProjectDetailsRepo.java
@@ -2,9 +2,18 @@ package com.msp.assignment.repository;
 
 
 import com.msp.assignment.model.ProjectsDetails;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProjectDetailsRepo extends JpaRepository<ProjectsDetails, Long> {
+    @EntityGraph(attributePaths = {"projects", "projects.users"})
+    Optional<ProjectsDetails> findById(Long id);
+
+    @EntityGraph(attributePaths = {"projects", "projects.users"})
+    List<ProjectsDetails> findAll();
 }
