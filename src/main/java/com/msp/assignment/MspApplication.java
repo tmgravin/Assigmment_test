a406c9536941074a947741d69ff8fa4501770482
RANAS7 <deepakrana.dr537@gmail.com>
2024-08-05 11:43:09 +0545

    setup .env file

--- src/main/java/com/msp/assignment/MspApplication.java
+++ src/main/java/com/msp/assignment/MspApplication.java
@@ -1,5 +1,6 @@
 package com.msp.assignment;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,6 +11,22 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.msp.assignment.repository")
 public class MspApplication {
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv dotenv = Dotenv.load();
+
+		// Set S3 Credential environment variables as system properties
+		System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
+		System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
+		System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
+		System.setProperty("S3_BUCKET_NAME", dotenv.get("S3_BUCKET_NAME"));
+
+		// Set Database Resource environment variables as system properties
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+
+
 		SpringApplication.run(MspApplication.class, args);
 	}
 
