fb5c8d9b19c745f1f62ac4121e4b8ecfcd18d5f4
RANAS7 <deepakrana.dr537@gmail.com>
2024-08-07 14:33:48 +0545

    change database Configration

--- src/main/java/com/msp/assignment/MspApplication.java
+++ src/main/java/com/msp/assignment/MspApplication.java
@@ -22,10 +22,13 @@ public class MspApplication {
 		System.setProperty("S3_BASE_URL", dotenv.get("S3_BASE_URL"));
 
 		// Set Database Resource environment variables as system properties
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+		System.setProperty("DB_HOST",dotenv.get("DB_HOST"));
+		System.setProperty("DATABASE", dotenv.get("DATABASE"));
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 
 		// Set Email Resource environment variables as system properties
 		System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
